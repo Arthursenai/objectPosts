fa90a41a1fc2b5f6f83b3de831c91d05100e7064
arthurborges <arthurborges.senai564@gmail.com>
2023-08-07 10:24:02 -0300

    first commit

---
+++ Aula1objeto/assets/js/script.js
@@ -0,0 +1,58 @@
+// let personsimple = 'fulano tal';
+
+// const person = {
+//     name: 'fulano',
+//     surname: 'tal',
+//     age: 23,
+//     eyecolor:'blue'
+// };
+
+
+//  let botao = document.getElementById("pqo");
+//  let quw;
+
+//   botao.addEventListener('click', function(){
+//      let quw = '09'
+//  });
+
+//     function yoda(){
+        
+//        if(quw = '09'){
+//         alert ('Você é um Yoda');
+//     }
+//     }
+
+
+// console.log(personsimple);
+// console.log(person);
+// console.log(person.age);
+// console.log(person.eyecolor);
+
+// function loadOut() {
+//     let firstName = document.getElementById("i1").value;
+//     let surName = document.getElementById("i2").value;
+//     let age = document.getElementById("i3").value;
+//     let eyeColor = document.getElementById("i4").value;
+//     let arrn = [];
+//     let arrs = [];
+//     let arrage = [];
+//     let arreye = [];
+//     let atual = 2023 - age;
+//     arrn.push(firstName);
+//     arrs.push(surName);
+//     arrage.push(age);
+//     arreye.push(eyeColor);
+//     const personObject = {
+
+//         name: arrn,
+//         surname: arrs,
+//         aged: arrage,
+//         years: atual,
+//         eyecolor: arreye
+//     }
+
+//     let ady = document.getElementById("loa");
+//     ady.innerHTML= "Bem vinde " + personObject.name + " " + personObject.surname + " " + "De " + personObject.years + " " + "a cor dos seus olhos é " + personObject.eyecolor;
+// }
+
+
---
+++ Revisão IMC/assets/js/script.js
@@ -0,0 +1,12 @@
+let input1 = Number(document.getElementById("inserir1"));
+let input2 = Number(document.getElementById("inserir2"));
+let result = document.getElementById("result");
+
+function calcularIMC(){
+    let imc;
+    if (input1 == "" || input2 == ""){
+        result.innerHTML='Preencha os campos corretamente';
+    }else{
+
+    }    
+}
